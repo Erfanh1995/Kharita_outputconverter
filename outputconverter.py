1ff95f4bcad4a983f8c1f3b0879d54b89c087549
Erfan Hosseini <erfanh1995@aol.com>
2020-08-04 03:04:11 -0500

    Update outputconverter.py
    ask for file path

--- outputconverter.py
+++ outputconverter.py
@@ -8,7 +8,8 @@ e = []
 it = 0
 temp = []
 tempf = []
-with open("data_uic_edges.txt", 'r') as f1:
+filename = input("What's the global path to the file?")
+with open(filename, 'r') as f1:
 	print("reading the file")
 	a = f1.readlines()
 	a = [elm for elm in a if elm != "\n"]
@@ -25,14 +26,14 @@ for i in range(it):
 	v_conv.insert(i,str(utm.from_latlon(tempf[0],tempf[1])[0]) + "," + str(utm.from_latlon(tempf[0],tempf[1])[1]) + "\n")
 	tempf = []
 
-with open("chicago_vertices.txt", 'a') as f2:
+with open("vertices.txt", 'a') as f2:
 	print("writing the vertices")
 	for i in range(it):
 		f2.write(str(i+1)+","+v_conv[i])
 f2.close()
 for j in range(len(a)//2):
 	e.insert(j,str(v.index(a[(2*j)])+1)+","+str(v.index(a[(2*j)+1])+1)+"\n")
-with open("chicago_edges.txt", 'a') as f3:
+with open("edges.txt", 'a') as f3:
 	print("writing the edges")
 	for k in range(len(e)):
 		f3.write(str(k+1) + "," + e[k])
